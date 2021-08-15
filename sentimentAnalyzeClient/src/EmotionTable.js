25c256264b2f4e4dcd29130c7f7f1d58b0ae70c1
Cookies au Lait <47886319+CookiesAuLait@users.noreply.github.com>
2021-08-15 16:22:36 +0800

    Add files via upload

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -18,51 +18,9 @@ class EmotionTable extends React.Component {
 			  </tbody>
 			</table>
 			</div>
-			// {JSON.stringify(this.props.emotions)}
-			// <table className="table table-bordered">
-			// <tbody>
-			// {
-			// 		this.props.emotions && Object.entries(this.props.emotions).map(function (element, val){
-			/* 	  return (
-					  <tr key={element}>
-						  <td>{element[0]}</td>
-						  <td>{element[1]}</td>
-					  </tr>
-				  ); */
-			// })
-			// }
-			// </tbody>
 			);
 		  }
-	// state = { emotions:[] }
-	// componentDidMount() {
-	
-	// 	let listOfkeywords = this.props.emotions.result.keywords;
-	// 	let listOfkeywordsAsArray = Object.entries(listOfkeywords);
-    //     let eventDetails = listOfkeywordsAsArray.map((keywordDetail)=>{
-	// 		let eventListCollection = Object.entries(keywordDetail[1])
-	// 		let emotion = Object.entries(eventListCollection[2][1])
-	// 		let emotionText = eventListCollection[0]
-	// 		return <tr><td style={{border: "1px solid black"}}>{emotionText[1]}
-	// 			<table width="100%"><tbody><tr><td>{ emotion[0][0]}</td><td>{ emotion[0][1]}</td></tr>
-	// 			<tr><td>{ emotion[1][0]}</td><td>{ emotion[1][1]}</td></tr>
-	// 			<tr><td>{ emotion[2][0]}</td><td>{ emotion[2][1]}</td></tr>
-	// 			<tr><td>{ emotion[3][0]}</td><td>{ emotion[3][1]}</td></tr>
-	// 			<tr><td>{ emotion[4][0]}</td><td>{ emotion[4][1]}</td></tr></tbody></table></td></tr>  
-	// 		})
-	// 	this.setState({emotions:<table className="table table-bordered"><tbody>{eventDetails}</tbody></table>})
-		
-	// }
-    // render() {
-    //   return (  
-	// 		<div>
-	// 			<br/>
-	// 				{
-	// 					this.state.emotions
-	// 				}
-	// 		</div>
-    //       );
-    // }
+
     
 }
 export default EmotionTable;
