900e8f968cde985a2aa0cae322115d3ebd690f8c
Cookies au Lait <47886319+CookiesAuLait@users.noreply.github.com>
2021-08-15 16:20:52 +0800

    Add files via upload

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -35,19 +35,12 @@ app.get("/url/emotion", (req,res) => {
 	console.log(req.query.url);
 	
 	const analyzeParams = {'url': req.query.url, 'features': {'keywords': {'emotion': true, 'sentiment': false, 'limit':5}}};
-//     nlu.analyze(analyzeParams).then(analysisResults => {
-//     	let result = JSON.stringify(analysisResults, null, 2);
-//     	console.log(result);
-//     	res.send(result);
-//     }).catch(err => {
-//     	console.log('error:', err);
-//     });
 	nlu.analyze(analyzeParams).then(analysisResults => {
 		res.status(200);
-		if analysisResults.result.keywords {
+		if (analysisResults.result.keywords) {
 				var dataFilter = analysisResults.result.keywords.sort((a, b) => a.count < b.count ? 1 : -1)[0];
 				console.log(dataFilter);
-				if dataFilter.emotion {
+				if (dataFilter.emotion) {
 					res.send(dataFilter.emotion);
 				} else {
 					res.send('Without data');
@@ -66,19 +59,12 @@ app.get("/url/sentiment", (req,res) => {
 	console.log(req.query.url);
 	
 	const analyzeParams = {'url': req.query.url, 'features': {'keywords': {'emotion': false, 'sentiment': true, 'limit':5}}};
-//     nlu.analyze(analyzeParams).then(analysisResults => {
-//     	let result = JSON.stringify(analysisResults, null, 2);
-// 		console.log(analysisResults.result.keywords[0].sentiment.label);
-// 		res.send(JSON.stringify(analysisResults.result.keywords[0].sentiment.label, null, 2));
-// }).catch(err => {
-// 		console.log('error:', err);
-// 	});
     nlu.analyze(analyzeParams).then(analysisResults => {
 		res.status(200);
-		if analysisResults.result.keywords {
+		if (analysisResults.result.keywords) {
 				var dataFilter = analysisResults.result.keywords.sort((a, b) => a.count < b.count ? 1 : -1)[0];
 				console.log(dataFilter);
-				if dataFilter.sentiment.label {
+				if (dataFilter.sentiment.label) {
 					res.send(dataFilter.sentiment.label);
 				} else {
 					res.send('Without data');
@@ -100,19 +86,12 @@ app.get("/text/emotion", (req,res) => {
 	console.log(req.query.text);
 	
 	const analyzeParams = {'text': req.query.text, 'features': {'keywords': {'emotion': true, 'sentiment': false, 'limit':5}}};
-// 	nlu.analyze(analyzeParams).then(analysisResults => {
-// 		let result = JSON.stringify(analysisResults, null, 2);
-// 		console.log(result);
-// 		res.send(result);
-// 	}).catch(err => {
-// 		console.log('error:', err);
-// 	});
     nlu.analyze(analyzeParams).then(analysisResults => {
 		res.status(200);
-		if analysisResults.result.keywords {
+		if (analysisResults.result.keywords) {
 				var dataFilter = analysisResults.result.keywords.sort((a, b) => a.count < b.count ? 1 : -1)[0];
 				console.log(dataFilter);
-				if dataFilter.emotion {
+				if (dataFilter.emotion) {
 					res.send(dataFilter.emotion);
 				} else {
 					res.send('Without data');
@@ -131,19 +110,12 @@ app.get("/text/sentiment", (req,res) => {
 	console.log(req.query.text);
 	
 	const analyzeParams = {'text': req.query.text, 'features': {'keywords': {'emotion': false, 'sentiment': true, 'limit':5}}};
-//     nlu.analyze(analyzeParams).then(analysisResults => {
-//     	let result = JSON.stringify(analysisResults, null, 2);
-// 		console.log(analysisResults.result.keywords[0].sentiment.label);
-// 		res.send(JSON.stringify(analysisResults.result.keywords[0].sentiment.label, null, 2));
-// }).catch(err => {
-// 		console.log('error:', err);
-// 	});
 	nlu.analyze(analyzeParams).then(analysisResults => {
 		res.status(200);
-		if analysisResults.result.keywords {
+		if (analysisResults.result.keywords) {
 				var dataFilter = analysisResults.result.keywords.sort((a, b) => a.count < b.count ? 1 : -1)[0];
 				console.log(dataFilter);
-				if dataFilter.sentiment.label {
+				if (dataFilter.sentiment.label) {
 					res.send(dataFilter.sentiment.label);
 				} else {
 					res.send('Without data');
